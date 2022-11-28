e25fb9b7d159c9b04f90e354d4696bfac3b43c38
Jeff Braun <jeffbr@Mac-Studio-von-Jeff.local>
2022-11-28 16:35:06 +0100

    Cleaning

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [formData, setFormData] = useState({});
+
+  // function sendData() {
+  //   fetch("http://localhost:3001/register", {
+  //     method: "POST",
+  //     body: JSON.stringify(),
+  //     headers: {
+
+  //     }
+  //   })
+  // };
+
+    function inputChange(e) {
+    setFormData({...formData, [e.target.name]: e.target.value});
+  };
+
   return (
     <div className="App">
-
+      <div>
+        <label>Quantity</label>
+        <input type="text" name="quantity" onChange={inputChange}></input>
+      </div>
+      <div>
+        <label>Product</label>
+        <input  type="text" name="product" onChange={inputChange}></input>
+      </div>
+      <div>
+        <label>User</label>
+        <input  type="text" name="user" onChange={inputChange}></input>
+      </div>
+      <div>
+        <button onClick={inputChange}>Submit</button>
+      </div>
     </div>
   );
 }
